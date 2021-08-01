6009e886c6aa35da9eaa1b2b276433cb3d15a473
DamianTab <dam.tab@wp.pl>
2021-08-01 15:31:57 +0200

    init project

---
+++ src/main/java/damian/tab/dsynchro/DSynchroApplication.java
@@ -0,0 +1,13 @@
+package damian.tab.dsynchro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSynchroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSynchroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/damian/tab/dsynchro/DSynchroApplicationTests.java
@@ -0,0 +1,13 @@
+package damian.tab.dsynchro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSynchroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
